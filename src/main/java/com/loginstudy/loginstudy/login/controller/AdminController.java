fd066231c1249dff623148fe44381d1ff2c8b827
hyungkishin <tkaqkeldk@nate.com>
2024-04-10 20:50:46 +0900

    feat: test Controller 구현

---
+++ src/main/java/com/loginstudy/loginstudy/login/controller/AdminController.java
@@ -0,0 +1,19 @@
+package com.loginstudy.loginstudy.login.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping
+    public String main() {
+        return "admin Controller";
+    }
+
+
+}
